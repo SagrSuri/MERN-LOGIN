9d8782df4e48fb1cf61404e1e1b2d4aa45af2831
SAGRSURI <sagarsuriofficial@gmail.com>
2024-06-15 13:20:11 +0530

    check to resolve

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from 'react'; // Import React from 'react' (useEffect is also imported here)
+import './App.css'; // Import your CSS file
+import { Routes, Route } from 'react-router-dom'; // Import Routes and Route from react-router-dom
 
-// Import your components
-import Home from './components/Home';
+// Import your components from their respective files
+import Home from './components/Home.js';
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
 import ResetPassword from './components/ResetPassword';
@@ -11,19 +12,17 @@ import PageNotFound from './components/PageNotFound';
 
 function App() {
   return (
-    <div className="App h-screen flex justify-center items-center">
-      <Router basename={process.env.PUBLIC_URL}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/reset-password/:resetPasswordToken" element={<ResetPassword />} />
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </Router>
+    <div className="App h-[100vh] flex justify-center items-center"> {/* Apply CSS classes */}
+      <Routes basename={process.env.PUBLIC_URL}> {/* Use Routes with basename from environment */}
+        <Route path="/" element={<Home />} /> {/* Define route for Home component */}
+        <Route path="/signin" element={<SignIn />} /> {/* Define route for SignIn component */}
+        <Route path="/signup" element={<SignUp />} /> {/* Define route for SignUp component */}
+        <Route path="/forgot-password" element={<ForgotPassword />} /> {/* Define route for ForgotPassword component */}
+        <Route path="/reset-password/:resetPasswordToken" element={<ResetPassword />} /> {/* Define route for ResetPassword component */}
+        <Route path="*" element={<PageNotFound />} /> {/* Define route for PageNotFound component */}
+      </Routes>
     </div>
   );
 }
 
-export default App;
+export default App; // Export the App component as the default export
